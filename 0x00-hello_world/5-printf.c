47b1fffb41f9dcdff20f2fbd4192e35ac46f72dd
Emmanuel Adaja <emmanueladaja@yahoo.com>
2023-02-16 19:01:13 -0800

    update to 5-ptintf.c solution

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,12 @@
 #include <stdio.h>   /*import standard input & output*/
 
 
+/**
+ * main function: Use printf to print to std output
+ * Return: returns 0 if no error in execution occurs
+ */
 int main(void)
 {
-	/*Use printf to print to std output*/
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
