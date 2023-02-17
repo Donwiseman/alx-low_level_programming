7c3124a05f921d776ab48bde9ea8d08455aaf22d
Emmanuel Adaja <emmanueladaja@yahoo.com>
2023-02-16 19:40:32 -0800

    4-puts.c solution

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>   /*import standard input & output*/
+
+
+/**
+ * main - Use puts() to print to std output
+ * Return: returns 0 if no error in execution occurs
+ */
+int main(void)
+{
+	char str[] = "Programming is like building a multilingual puzzle";
+
+	puts(str);
+	return (0);
+}
