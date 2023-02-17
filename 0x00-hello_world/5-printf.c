4d1d5cf14934eec16fac5d3336af379395956fb5
Emmanuel Adaja <emmanueladaja@yahoo.com>
2023-02-16 18:48:12 -0800

    5-printf.c solution

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>   /*import standard input & output*/
+
+
+int main(void)
+{
+	/*Use printf to print to std output*/
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
