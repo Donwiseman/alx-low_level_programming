f7522f1c29a1e58cb27afc82460dfd84fb1f473d
Emmanuel Adaja <emmanueladaja@yahoo.com>
2023-02-20 17:02:30 -0800

    9-print_comb.c solution

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit of decimal numbers seperated by a comma
+ *
+ * Return: returns 0 if function runs with no errors
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar(48 + n);
+		if (n != 9)
+			putchar(44);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
