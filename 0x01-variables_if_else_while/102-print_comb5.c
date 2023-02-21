d85a2078153d83a3852f35d8294eca1485a0a1f0
Emmanuel Adaja <emmanueladaja@yahoo.com>
2023-02-20 19:56:44 -0800

    102-print_comb5.c solution

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of a two two_digit numbers
+ *
+ * Return: 0 if it executes with no error
+ */
+int main(void)
+{
+	int loop1a = 0;
+
+	while (loop1a < 10)
+	{
+		int loop1b = 0;
+
+		while (loop1b < 10)
+		{
+			int loop2a = loop1a;
+
+			while (loop2a < 10)
+			{
+				/*to avoid repitition with first 2-digit*/
+				int loop2b = loop1b + 1;
+
+				while (loop2b < 10)
+				{
+					/*print first two digit and a space*/
+					putchar(48 + loop1a);
+					putchar(48 + loop1b);
+					putchar(32);
+					/*print 2nd two digit number*/
+					putchar(48 + loop2a);
+					putchar(48 + loop2b);
+					/*condition to handle last-num comma*/
+					if (!(loop1a == 9 && loop1b == 8))
+					{
+						putchar(44); /*print a comma*/
+						putchar(32); /*print a space*/
+					}
+					loop2b++;
+				}
+				loop2a++;
+			}
+			loop1b++;
+		}
+		loop1a++;
+	}
+	putchar('\n');
+	return (0);
+}
