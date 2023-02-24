b6e5fc0e688df526014a9751a7d5ffddfce9ce73
Emmanuel Adaja <emmanueladaja@yahoo.com>
2023-02-23 21:03:09 -0800

    9-fizz_buzz.c update

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - prints number 1 to 100 replacing multiples of 3 with Fizz, 5 with Buzz and FizzBuzz for multiples of 3 and 5.
+ * main - prints number 1 to 100 replacing multiples of 3 with Fizz, 5 with
+ Buzz and FizzBuzz for multiples of 3 and 5.
  *
  * Return: 0 if it executes with no error
  */
 int main(void)
 {
 	int a;
+
 	for (a = 1; a <= 100; a++)
 	{
 		if (a % 15 == 0)
