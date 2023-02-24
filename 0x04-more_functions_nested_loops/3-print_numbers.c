a3a71599b393f175dd2d586ff9e580e4f3cee495
Emmanuel Adaja <emmanueladaja@yahoo.com>
2023-02-23 19:03:16 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints all single digit of decimal numbers
+ *
+ * Return: returns 0 if function runs with no errors
+ */
+void print_numbers(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		_putchar(48 + n);
+		n++;
+	}
+	_putchar('\n');
+	return (0);
+}
