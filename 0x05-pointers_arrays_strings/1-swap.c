6f1eec887baba6e4151714c5ac815592d8545a27
Emmanuel Adaja <emmanueladaja@yahoo.com>
2023-02-28 20:04:24 -0800

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+/**
+ * swap_int - swaps the values of two given integer prameters
+ * @a: pointer parameter that points to an int
+ * @b: another pointer parameter
+ */
+void swap_int(int *a, int *b)
+{
+	int *x;
+
+	x = a;
+	a = b;
+	b = x;
+}
