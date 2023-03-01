8a079a9a048935e07f8d33834cc19f79462ee801
Emmanuel Adaja <emmanueladaja@yahoo.com>
2023-02-28 20:55:39 -0800

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include <stdio>
+
+/**
+ * print_array - prints n elements of an array of integers
+ * @n: number of elements to print
+ */
+void print_array(int *a, int n)
+{
+	int x;
+
+	for (x = 0; x < n; x++)
+	{
+		printf("%d", *(a + x));
+		if (x != n - 1)
+			printf(", ");
+	}
+	printf("\n");
+}
