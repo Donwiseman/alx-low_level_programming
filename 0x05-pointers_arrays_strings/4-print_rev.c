1a20e31ea9567c32aa7c96251d35635dc8edf419
Emmanuel Adaja <emmanueladaja@yahoo.com>
2023-03-01 18:59:40 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev -  write a string to the stdout
+ * @s: pointer to a string
+ */
+void print_rev(char *s)
+{
+	char *p = s;
+
+	while (1)
+	{
+		if (!*p)
+			break;
+		p++;
+	}
+	while (1)
+	{
+		p--;
+		_putchar(*p);
+		if (p == s)
+			break;
+	}
+	_putchar('\n');
+}
