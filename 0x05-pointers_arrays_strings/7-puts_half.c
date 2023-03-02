08e8043d87452197eb24c131d4c22625d3aa34e0
Emmanuel Adaja <emmanueladaja@yahoo.com>
2023-03-02 05:03:51 -0800

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - prints later half of a string to stdout
+ * @str: pointer to a string
+ */
+void puts_half(char *str)
+{
+	char *p = str;
+	int len = 0;
+	int count;
+
+	while (*str)
+	{
+		if (!*p)
+			break;
+		p++;
+		len++;
+	}
+	for (count = 0; count < (len / 2); count++)
+	{
+		p--;
+		_putchar(*p);
+	}
+	_putchar('\n');
+}
