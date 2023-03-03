622e61e131f5302bb7f72292e4f50a5b36f7fc9e
Emmanuel Adaja <emmanueladaja@yahoo.com>
2023-03-02 21:17:26 -0800

    2-strncpy.c1

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,14 +10,22 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	char *p = dest;
 	int x;
+	int end = 0;
 
 	for (x = 0; x < n; x++)
 	{
-		*p = *src;
 		if (!*src)
-			break;
+			end = 1;
+		if (end == 0)
+		{
+			*p = *src;
+			src++;
+		}
+		else
+		{
+			*p = '\0';
+		}
 		p++;
-		src++;
 	}
 	return (dest);
 }
