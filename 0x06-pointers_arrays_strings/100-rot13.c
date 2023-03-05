451de5853070f1e679480a1bfe128a82cacca521
Emmanuel Adaja <emmanueladaja@yahoo.com>
2023-03-05 07:27:07 -0800

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+/**
+ * rot13 - encodes a string into ROT13
+ * @s: string to be encoded
+ *
+ * Return: returns a pointer to the string
+ */
+char *rot13(char *s)
+{
+	char *p = s;
+
+	while (*p)
+	{
+		while ((*p >= 65 && *p <= 90) || (*p >= 97 && *p <= 122))
+		{
+			if ((*p >= 65 && *p <= 77) || (*p >= 97 && *p <= 109))
+				*p += 13;
+			else
+				*p -= 13;
+			break;
+		}
+		p++;
+	}
+	return (s);
+}
