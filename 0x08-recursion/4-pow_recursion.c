92e150b6efbf2b97245f1b04b1fae547559823bf
Emmanuel Adaja <emmanueladaja@yahoo.com>
2023-03-08 13:02:08 -0800

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+/**
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: base value to be operated on
+ * @y: power to be raised to
+ *
+ * Return: the value of x raised to power y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+	{
+		if (x < 0)
+			return (-1);
+		else
+			return (1);
+	}
+	if (y == 1)
+		return (x);
+	return (x * _pow_recursion(x, (y - 1)));
+}
