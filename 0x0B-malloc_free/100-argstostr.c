78d6535d873d0e96e1f00dba61512a994c5bf209
Emmanuel Adaja <emmanueladaja@yahoo.com>
2023-03-18 09:32:57 -0700

    100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,70 @@
+#include <stdlib.h>
+
+/**
+ * _strlen -  returns the length of a string
+ * @s: pointer to a string
+ *
+ * Return: returns the length of the given string minus the terminating string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+	char *p = s;
+
+	while (1)
+	{
+		if (!*p)
+			break;
+		p++;
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * argstostr - concatenates all the arguments of your program
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: returns a pointer to the concantenated string
+ */
+char *argstostr(int ac, char **av)
+{
+	int len;
+	int count;
+	char *concat;
+	char *p;
+	int x = 0;
+
+	if (ac == 0 ||  av == NULL)
+		return (NULL);
+	for (count = 0; count < ac; count++)
+	{
+		len += _strlen(av[count]) + 1;
+	}
+	count = 0;
+	concat = malloc(len);
+	if (concat == NULL)
+		return (NULL);
+	while (count < ac)
+	{
+		p = av[count];
+		while (1)
+		{
+			if (*p)
+			{
+				concat[x] = *p;
+				p++;
+				x++;
+			}
+			else
+			{
+				concat[x] = '\n';
+				x++;
+				break;
+			}
+		}
+		count++;
+	}
+	return (concat);
+}
