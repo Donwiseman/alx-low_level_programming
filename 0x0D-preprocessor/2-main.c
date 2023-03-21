b473728e83b704f9d155efc9ef0e28fe996dc03f
Emmanuel Adaja <emmanueladaja@yahoo.com>
2023-03-21 16:14:43 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints out the name of the source file compiled from
+ *
+ * Return: 0 if runs with no error
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
