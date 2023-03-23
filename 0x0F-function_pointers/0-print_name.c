4c41907591bb5d7b3a665dc69d4d4b36825b546e
Emmanuel Adaja <emmanueladaja@yahoo.com>
2023-03-22 19:00:41 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,9 @@
+/**
+ * print_name - prints the name which is passed
+ * @name: a string representing the name
+ * @f: pointer to a function to be used to print the name normally or in Caps
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+Folder containing various source codes involving the use of pointers to a function in C language.
