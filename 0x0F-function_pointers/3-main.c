5f19d1283ba3d4cd488bf4ed1d054376a26523a0
Emmanuel Adaja <emmanueladaja@yahoo.com>
2023-03-24 21:52:16 -0700

    solution for 3-calc

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,43 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - calculator which prints the value for two int values in an addition,
+ * subtraction, division, multiplication or modulus operation or Error.
+ * @argv: argument vector
+ * @argc: argument count
+ * Return: 0 if succesful, 98 if wrong argument number, 99 if wrong operator
+ * and 100 if error is from using 0 for a divide or modulus operation.
+ */
+int main(int argc, char *argv[])
+{
+	int a, b;
+	char *s;
+	int (*op_p)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+		return (98);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	s = argv[2];
+	if ((*s == '/' || *s == '%') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+		return (100);
+	}
+	op_p = get_op_func(s);
+	if (op_p == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+		return (99);
+	}
+	printf("%d\n",(op_p(a, b)));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+/**
+ * op_add - add the two int arguments passed
+ * @a: first int parameter
+ * @b: second int parameter
+ *
+ * Return: returns the addition of the arguments
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - add the two int arguments passed
+ * @a: first int parameter
+ * @b: second int parameter
+ *
+ * Return: returns the subtraction of the arguments
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - add the two int arguments passed
+ * @a: first int parameter
+ * @b: second int parameter
+ *
+ * Return: returns the multiplication of the arguments
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - add the two int arguments passed
+ * @a: first int parameter
+ * @b: second int parameter
+ *
+ * Return: returns the division of the arguments
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - add the two int arguments passed
+ * @a: first int parameter
+ * @b: second int parameter
+ *
+ * Return: returns the modulus of the arguments
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
