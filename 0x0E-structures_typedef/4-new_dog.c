bcef51a7f9f75a6b125f463a90ef23576dde1a5a
Emmanuel Adaja <emmanueladaja@yahoo.com>
2023-03-25 12:20:59 -0700

    4-new_dog.c in 0x0E

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,47 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - creates a new dog type
+ * @name: name of dog
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Return: returns a pointer to the created dog type
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	char *name_copy;
+	char *owner_copy;
+	int name_len = 0, owner_len = 0, index;
+	dog_t new_dog;
+	dog_t *p;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	while (*(name + name_len))
+		name_len++;
+	while (*(owner + owner_len))
+		owner_len++;
+	name_copy = malloc((name_len + 1) * sizeof(char));
+	owner_copy = malloc((owner_len + 1) * sizeof(char));
+	if (!name_copy)
+		return (NULL);
+	if (!owner_copy)
+		return (NULL);
+	/*copy name using for loop*/
+	for (index = 0; index <= name_len; index++)
+	{
+		*(name_copy + index) = *(name + index);
+	}
+	/*copy owner using for loop*/
+	for (index = 0; index <= owner_len; index++)
+	{
+		*(owner_copy + index) = *(owner + index);
+	}
+	new_dog.name = name_copy;
+	new_dog.age = age;
+	new_dog.owner = owner_copy;
+	p = &new_dog;
+	return (p);
+}
