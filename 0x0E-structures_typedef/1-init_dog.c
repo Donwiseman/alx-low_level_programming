1ff8f47195737dc1e3daf3a36d3938009d9526a6
Emmanuel Adaja <emmanueladaja@yahoo.com>
2023-03-25 08:42:52 -0700

    1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes a dog struct type
+ * @d: pointer to a dog type to be initialized
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner's name
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
