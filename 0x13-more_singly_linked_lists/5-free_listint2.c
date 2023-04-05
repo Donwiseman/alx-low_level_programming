2ec5497754a67d4625e1106f7a5e5dc8a1d5b02c
Emmanuel Adaja <emmanueladaja@yahoo.com>
2023-04-05 03:53:37 -0700

    5-free_listint2.c update to handle NULL input properly error handling

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -27,9 +27,9 @@ void free_listint2(listint_t **head)
 	listint_t *temp;
 
 	if (head == NULL)
-		*temp = NULL;
+		temp = NULL;
 	else
-		*temp = *head;
+		temp = *head;
 	if (temp && temp->next == NULL)
 	{
 		free(temp);
