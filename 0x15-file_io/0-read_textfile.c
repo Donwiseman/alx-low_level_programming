c2477909c79905849b0ffd7180d031be02c6fde0
Emmanuel Adaja <emmanueladaja@yahoo.com>
2023-04-08 23:31:01 -0700

    0-read_textfile.c in 0x15

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: pathname to file
+ * @letters: letters or characters to read
+ *
+ * Return: returns the actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t n, fd;
+	char *buf = (char *) malloc(letters);
+
+	if (filename == NULL || buf == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	n = read(fd, buf, letters);
+	if (n < 1)
+	{
+		free(buf);
+		return (0);
+	}
+	if ((write(STDOUT_FILENO, buf, n)) < n)
+	{
+		free(buf);
+		return (0);
+	}
+	free(buf);
+	close(fd);
+	return (n);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+A repo containing various source codes incolving file input and output handling using functions from the fcntl.h and unistd.h headers.
