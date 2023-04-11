71624f344516217d882874e69414847324175fba
Emmanuel Adaja <emmanueladaja@yahoo.com>
2023-04-11 10:35:39 -0700

    3-set_bit.c update to cater for extremely long index

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -12,7 +12,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int mask;
 
-	if (n == NULL)
+	if (n == NULL || index > ((sizeof(unsigned long int) * 8) - 1))
 		return (-1);
 	mask = 1 << index;
 	*n = *n | mask;
