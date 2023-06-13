dc920b7bd20da739f314a342dde0820d5260b550
Emmanuel Adaja <emmanueladaja@yahoo.com>
2023-06-12 20:16:19 -0700

    4-hash_table_get.c update for null hash table

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,7 +12,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *head;
 
-	if (key == NULL || strcmp(key, "") == 0)
+	if (ht == NULL || key == NULL || strcmp(key, "") == 0)
 		return (NULL);
 	index = key_index((unsigned char *)key, ht->size);
 	head = ht->array[index];
